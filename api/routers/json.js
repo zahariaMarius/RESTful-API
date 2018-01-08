4b8b74b68db4bbcafb979ece43a41b42f42b0d1f
zahrsino <zaharialorenzo@gmail.com>
2018-01-08 19:18:12 +0100

    tutto funzionante

--- api/routers/json.js
+++ api/routers/json.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const fs = require('fs');
 const router = express.Router();
+const fs = require('fs');
+const uniqueIdGenerator = require('unique-id-generator');
 
 /**
  * normal get without id
  */
 router.get('/', (req, res, next) => {
-    fs.readFile('./jsonData/data.json',  function(error, data) {
+    fs.readFile('./jsonData/data.json', (error, data) => {
         res.status(200).json({
             message: "GET request handlers",
             datiLetti: JSON.parse(data)
@@ -19,40 +20,48 @@ router.get('/', (req, res, next) => {
  */
 router.get('/:jsonID', (req, res, next) => {
     const jsonID = req.params.jsonID;
-    fs.readFile('./jsonData/'+jsonID+'.json',  function(error, data) {
+    fs.readFile('./jsonData/'+jsonID+'.json', (error, data) => {
         if (error) {
-            res.status(404).json({})
+            setResponseMessage(res, 404);
         } else {
-            res.status(200).json({
-                message: "GET request handlers",
-                datiLetti: JSON.parse(data)
-            })
+            setResponseMessage(res, 200, {
+                message: "GET request successfully end",
+                object: JSON.parse(data)
+            });
+        }
+    });
+});
+
+router.post('/', (req, res, next) => {
+    var jsonID = uniqueIdGenerator({prefix:"id-"});
+    fs.writeFile('./jsonData/'+jsonID+'.json', "First creation", (error) => {
+        if (error) { throw error } else {
+            setResponseMessage(res, 201, {
+                message: "POST request successfully end",
+                howToUse: "Make your request to /json/yourID",
+                yourID: jsonID
+            });
         }
     });
 });
 
 /**
- * normal post without id
+ * normal put without id
  */
-router.post('/', (req, res, next) => {
-    const jsonData = {
-        nome: req.body.nome,
-        cognome: req.body.cognome
-    };
-    res.status(201).json({
-        message: "POST request handle",
-        object: jsonData
-    })
-});
-
-/**
- * patch with json ID
- */
-router.patch('/:jsonID', (req, res, next) => {
+router.put('/:jsonID', (req, res, next) => {
     const jsonID = req.params.jsonID;
-    res.status(200).json({
-        message: "PATCH request handle"
-    })
+    if (fs.existsSync('./jsonData/'+jsonID+'.json')) {
+        fs.writeFile('./jsonData/'+jsonID+'.json', JSON.stringify(req.body), (error) => {
+            if (error) { throw error } else {
+                setResponseMessage(res, 200, {
+                    message: "PUT request successfully end",
+                    object: req.body
+                });
+            }
+        });   
+    } else {
+        setResponseMessage(res, 404);
+    }
 });
 
 /**
@@ -64,5 +73,15 @@ router.delete('/:jsonID', (req, res, next) => {
         message: "DELETE request handle"
     })
 });
+
+/**
+ * function that set the return message
+ * @param {Response} res
+ * @param {INT} status 
+ * @param {STRING} message 
+ */
+function setResponseMessage(res, status, message) {
+    res.status(status).json(message)
+}
 
 module.exports = router;
