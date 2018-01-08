78818096b49f15e1509a73a8b7e97252a46f6d94
zahrsino <zaharialorenzo@gmail.com>
2018-01-08 13:34:18 +0100

    punto 8 funzionante

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('X-Powered-By', 'Node.js, Express');
-    res.header('X-Version', '1.0.0');
+    res.header('X-Version', '1.0.1');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
         return res.status(200).json({});
